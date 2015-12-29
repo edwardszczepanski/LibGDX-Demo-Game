0dfeb9ab37121eb1a95e140fa91b8180e36134f0
edwardszczepanski <eszczepa@uoregon.edu>
2015-12-29 12:41:28 -0800

    Updated hud

--- Nine Circles/core/src/com/edwardszczepanski/ninecircles/Scenes/Hud.java
+++ Nine Circles/core/src/com/edwardszczepanski/ninecircles/Scenes/Hud.java
@@ -44,9 +44,9 @@ public class Hud implements Disposable{
         countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
         scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
         timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        levelLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        marioLabel = new Label("MARIO", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        levelLabel = new Label("WASTE LAND", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        worldLabel = new Label("STAGE:", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        marioLabel = new Label("NINE CIRCLES", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
         table.add(marioLabel).expandX().padTop(10); // This expand X makes everything in the row share the row equally
         table.add(worldLabel).expandX().padTop(10);
