defad5778a4fd6ec406421819695f11bd9a7928e
edwardszczepanski <eszczepa@uoregon.edu>
2016-01-10 17:08:49 -0800

    New bitmap fonts added to repository.

--- Nine Circles/core/src/com/edwardszczepanski/ninecircles/Screens/SplashScreen.java
+++ Nine Circles/core/src/com/edwardszczepanski/ninecircles/Screens/SplashScreen.java
@@ -46,7 +46,7 @@ public class SplashScreen implements Screen{
         Tween.to(splash, SpriteAccessor.ALPHA, 2).target(1).repeatYoyo(1, 1.5f).setCallback(new TweenCallback() {
             @Override
             public void onEvent(int type, BaseTween<?> source) {
-                game.setScreen(new PlayScreen(game));
+                game.setScreen(new MenuScreen(game));
             }
         }).start(tweenManager);
     }
